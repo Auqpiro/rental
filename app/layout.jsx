c46a4967c589f0c1d3ca0291033c864dd578f133
Ivan Konovalov <zzoomzoone@gmail.com>
2024-05-14 20:25:43 -0300

    create navbar

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,17 +1,24 @@
+import Navbar from "@/components/Navbar";
+
 import "@/assets/styles/globals.css";
 
 export const metadata = {
   title: "Property Rental",
   description: "Find The Perfect Rental",
-  keywords: "rental, property rental, find properties"
+  keywords: "rental, property rental, find properties",
 };
 
 const MainLayout = ({ children }) => {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <header>
+          <Navbar />
+        </header>
+        <main>{children}</main>
+      </body>
     </html>
-  )
-}
+  );
+};
 
-export default MainLayout
+export default MainLayout;
